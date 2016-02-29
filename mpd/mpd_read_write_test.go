92db4c5a850e2ae6e4d6fce9f39a58f972611e86
Julian Cooper <jcooper@brightcove.com>
2016-02-29 12:52:43 -0800

    Fixed import

--- mpd/mpd_read_write_test.go
+++ mpd/mpd_read_write_test.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
-	"github.com/zencoder/go-dash/helpers"
+	"github.com/zencoder/go-dash/helpers/ptrs"
 	"github.com/zencoder/go-dash/helpers/testfixtures"
 )
 
