ef00537cdbdfc1e488b4940135e48a1784da59b3
Bobby Peck <rpeck@brightcove.com>
2017-08-10 15:21:10 -0700

    Add PresentationTimeOffset to SegmentTemplate

--- mpd/mpd.go
+++ mpd/mpd.go
@@ -148,13 +148,14 @@ type Role struct {
 
 // Segment Template is for Live Profile Only
 type SegmentTemplate struct {
-	AdaptationSet   *AdaptationSet   `xml:"-"`
-	SegmentTimeline *SegmentTimeline `xml:"SegmentTimeline,omitempty"`
-	Duration        *int64           `xml:"duration,attr"`
-	Initialization  *string          `xml:"initialization,attr"`
-	Media           *string          `xml:"media,attr"`
-	StartNumber     *int64           `xml:"startNumber,attr"`
-	Timescale       *int64           `xml:"timescale,attr"`
+	AdaptationSet          *AdaptationSet   `xml:"-"`
+	SegmentTimeline        *SegmentTimeline `xml:"SegmentTimeline,omitempty"`
+	PresentationTimeOffset *uint64          `xml:"presentationTimeOffset,attr,omitempty"`
+	Duration               *int64           `xml:"duration,attr"`
+	Initialization         *string          `xml:"initialization,attr"`
+	Media                  *string          `xml:"media,attr"`
+	StartNumber            *int64           `xml:"startNumber,attr"`
+	Timescale              *int64           `xml:"timescale,attr"`
 }
 
 type Representation struct {
