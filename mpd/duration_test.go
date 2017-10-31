59ce5aab1fc7b8ddb8af6d52ca8352efe4304022
Thom Shutt <thomshutt@users.noreply.github.com>
2017-10-31 11:54:34 +0000

    Add Error test cases and simplify duration parsing logic

--- mpd/duration_test.go
+++ mpd/duration_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestDuration(t *testing.T) {
@@ -15,28 +15,45 @@ func TestDuration(t *testing.T) {
 	}
 	for ins, ex := range in {
 		timeDur, err := time.ParseDuration(ins)
-		assert.Equal(t, nil, err)
+		require.Equal(t, nil, err)
 		dur := Duration(timeDur)
-		assert.Equal(t, ex, dur.String())
+		require.Equal(t, ex, dur.String())
 	}
 }
 
 func TestParseDuration(t *testing.T) {
 	in := map[string]float64{
-		"PT0S":         0,
-		"PT1M":         60,
-		"PT2H":         7200,
-		"PT6M16S":      376,
-		"PT1.97S":      1.97,
-		"PT1H2M3.456S": 3723.456,
-		"P1DT2H":       (26 * time.Hour).Seconds(),
-		"PT20M":        (20 * time.Minute).Seconds(),
-		"-P60D":        -(60 * 24 * time.Hour).Seconds(),
-		"PT1M30.5S":    (time.Minute + 30*time.Second + 500*time.Millisecond).Seconds(),
+		"PT0S":          0,
+		"PT1M":          60,
+		"PT2H":          7200,
+		"PT6M16S":       376,
+		"PT1.97S":       1.97,
+		"PT1H2M3.456S":  3723.456,
+		"P1DT2H":        (26 * time.Hour).Seconds(),
+		"PT20M":         (20 * time.Minute).Seconds(),
+		"PT1M30.5S":     (time.Minute + 30*time.Second + 500*time.Millisecond).Seconds(),
+		"PT1004199059S": (1004199059 * time.Second).Seconds(),
 	}
 	for ins, ex := range in {
 		act, err := parseDuration(ins)
-		assert.NoError(t, err, ins)
-		assert.Equal(t, ex, act.Seconds(), ins)
+		require.NoError(t, err, ins)
+		require.Equal(t, ex, act.Seconds(), ins)
+	}
+}
+
+func TestParseBadDurations(t *testing.T) {
+	in := map[string]string{
+		"P20M":   `Duration must be in the format: P[nD][T[nH][nM][nS]]`, // We don't allow Months (doesn't make sense when converting to duration)
+		"P20Y":   `Duration must be in the format: P[nD][T[nH][nM][nS]]`, // We don't allow Years (doesn't make sense when converting to duration)
+		"P15.5D": `Duration must be in the format: P[nD][T[nH][nM][nS]]`, // Only seconds can be expressed as a decimal
+		"P2H":    `Duration must be in the format: P[nD][T[nH][nM][nS]]`, // "T" must be present to separate days and hours
+		"2DT1H":  `Duration must be in the format: P[nD][T[nH][nM][nS]]`, // "P" must always be present
+		"PT2M1H": `Duration must be in the format: P[nD][T[nH][nM][nS]]`, // Hours must appear before Minutes
+		"P":      `At least one number and designator are required`,      // At least one number and designator are required
+		"-P20H":  `Duration cannot be negative`,                          // Negative duration doesn't make sense
+	}
+	for ins, msg := range in {
+		_, err := parseDuration(ins)
+		require.EqualError(t, err, msg, "Expected an error for: %s", ins)
 	}
 }
