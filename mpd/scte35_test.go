3754381e7e04cc1d8ff225cf70e406aadf5a73e3
Fernando Partida <ferpart@hotmail.com>
2025-01-07 16:05:51 -0600

    chore: update scte35 test to use a fixture.

--- mpd/scte35_test.go
+++ mpd/scte35_test.go
@@ -1,10 +1,10 @@
 package mpd
 
 import (
+	"os"
 	"strconv"
 	"testing"
 
-	. "github.com/zencoder/go-dash/v3/helpers/ptrs"
 	"github.com/zencoder/go-dash/v3/helpers/require"
 )
 
@@ -22,77 +22,11 @@ func TestPeriod_AddNewSCTE35Break(t *testing.T) {
 	m.Periods[1].AddNewSCTE35Break(VALID_EVENT_STREAM_TIMESCALE, 20000, "3", "/DAvAAAAAAAAAP/wFAUAAAsgf/+AAAAAAAcT/w==")
 	m.Periods[1].AddNewSCTE35Break(VALID_EVENT_STREAM_TIMESCALE, 15000, "2", "")
 
-	expectedMPD := &MPD{
-		XMLNs:                     Strptr("urn:mpeg:dash:schema:mpd:2011"),
-		Profiles:                  Strptr((string)(DASH_PROFILE_LIVE)),
-		Type:                      Strptr("static"),
-		MediaPresentationDuration: Strptr(VALID_MEDIA_PRESENTATION_DURATION),
-		MinBufferTime:             Strptr(VALID_MIN_BUFFER_TIME),
-		AvailabilityStartTime:     Strptr(VALID_AVAILABILITY_START_TIME),
-		period:                    nil,
-		Periods: []*Period{
-			{ID: "0"},
-			{ID: "1", EventStreams: []EventStream{
-				{
-					Timescale:   Uintptr(VALID_EVENT_STREAM_TIMESCALE),
-					SchemeIDURI: Strptr(SCTE352014SchemaUri),
-					Events: []Event{
-						{
-							ID:               Strptr("1"),
-							PresentationTime: Uint64ptr(10000),
-							Signals: []Signal{
-								{
-									XMLNs: Strptr(SCTE352014SchemaUri),
-									Binaries: []Binary{
-										{
-											XMLNs:      Strptr(SCTE352014SchemaUri),
-											BinaryData: Strptr(""),
-										},
-									},
-								},
-							},
-						},
-						{
-							ID:               Strptr("2"),
-							PresentationTime: Uint64ptr(15000),
-							Signals: []Signal{
-								{
-									XMLNs: Strptr(SCTE352014SchemaUri),
-									Binaries: []Binary{
-										{
-											XMLNs:      Strptr(SCTE352014SchemaUri),
-											BinaryData: Strptr(""),
-										},
-									},
-								},
-							},
-						},
-						{
-							ID:               Strptr("3"),
-							PresentationTime: Uint64ptr(20000),
-							Signals: []Signal{
-								{
-									XMLNs: Strptr(SCTE352014SchemaUri),
-									Binaries: []Binary{
-										{
-											XMLNs:      Strptr(SCTE352014SchemaUri),
-											BinaryData: Strptr("/DAvAAAAAAAAAP/wFAUAAAsgf/+AAAAAAAcT/w=="),
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			}},
-			{ID: "2"},
-		},
-	}
-
-	expectedString, err := expectedMPD.WriteToString()
-	require.NoError(t, err)
-	actualString, err := m.WriteToString()
+	expected, err := os.ReadFile("fixtures/scte35.mpd")
 	require.NoError(t, err)
 
-	require.EqualString(t, expectedString, actualString)
+	actual, err := m.WriteToString()
+	require.NoError(t, err)
+
+	require.EqualString(t, string(expected), actual)
 }
