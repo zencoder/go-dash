6d579508b0dc4ec8819b12971dd7a81c014683eb
Fernando Partida <ferpart@hotmail.com>
2025-01-09 14:19:28 -0600

    chore: add tests for options update

--- mpd/scte35_test.go
+++ mpd/scte35_test.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 	"testing"
 
+	"github.com/Comcast/scte35-go/pkg/scte35"
 	"github.com/zencoder/go-dash/v3/helpers/require"
 )
 
@@ -18,9 +19,11 @@ func TestPeriod_AddNewSCTE35Break(t *testing.T) {
 		period.ID = strconv.Itoa(i)
 	}
 
-	m.Periods[1].AddNewSCTE35Break(VALID_EVENT_STREAM_TIMESCALE, 10000, "1", "")
-	m.Periods[1].AddNewSCTE35Break(VALID_EVENT_STREAM_TIMESCALE, 20000, "3", "/DAvAAAAAAAAAP/wFAUAAAsgf/+AAAAAAAcT/w==")
-	m.Periods[1].AddNewSCTE35Break(VALID_EVENT_STREAM_TIMESCALE, 15000, "2", "")
+	m.Periods[1].AddNewSCTE35Break(VALID_EVENT_STREAM_TIMESCALE, 10000, "1")
+
+	m.Periods[1].AddNewSCTE35Break(1, 20000, "3", WithNameSpace(SCTE35352016Namespace), WithBodyBinary("/DAgAAAAAAAAAP/wBQb+AABb0AAAABAAAAEAAQCA=="))
+	m.Periods[1].AddNewSCTE35Break(1, 20001, "4", WithNameSpace(SCTE35352016Namespace), WithSpliceInsertCommand())
+	m.Periods[1].AddNewSCTE35Break(VALID_EVENT_STREAM_TIMESCALE, 15000, "2", WithSpliceInfoSection(&scte35.SpliceInfoSection{SpliceCommand: scte35.NewSpliceCommand(scte35.SpliceScheduleType)}))
 
 	expected, err := os.ReadFile("fixtures/scte35.mpd")
 	require.NoError(t, err)
