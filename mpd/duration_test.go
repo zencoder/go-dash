f656b0ec1507a03bb804f46feeb1f0da84ae4fbf
Fernando Partida Milanés <ferpart@hotmail.com>
2025-02-05 09:20:21 -0600

    Parse durations of less than a second (#109)
    * feat: Parse durations of less than a second.
    
    * chore: add test for less than second parsing
    
    * chore: rename func
    
    * chore: fix IDE warns
    
    * chore: to lower start of error messages
    
    * feat: Add chrono library
    
    * feat: Replace manual duration parsing with chrono parsing
    
    * chore: correct failing tests

--- mpd/duration_test.go
+++ mpd/duration_test.go
@@ -10,15 +10,19 @@ import (
 
 func TestDuration(t *testing.T) {
 	in := map[string]string{
+		"0.5ms": "PT0.0005S",
+		"7ms":   "PT0.007S",
 		"0s":    "PT0S",
 		"6m16s": "PT6M16S",
 		"1.97s": "PT1.97S",
 	}
 	for ins, ex := range in {
-		timeDur, err := time.ParseDuration(ins)
-		require.NoError(t, err)
-		dur := Duration(timeDur)
-		require.EqualString(t, ex, dur.String())
+		t.Run(ins, func(t *testing.T) {
+			timeDur, err := time.ParseDuration(ins)
+			require.NoError(t, err)
+			dur := Duration(timeDur)
+			require.EqualString(t, ex, dur.String())
+		})
 	}
 }
 
@@ -34,27 +38,31 @@ func TestParseDuration(t *testing.T) {
 		"PT20M":         (20 * time.Minute).Seconds(),
 		"PT1M30.5S":     (time.Minute + 30*time.Second + 500*time.Millisecond).Seconds(),
 		"PT1004199059S": (1004199059 * time.Second).Seconds(),
+		"PT2M1H":        (time.Minute*2 + time.Hour).Seconds(),
 	}
 	for ins, ex := range in {
-		act, err := ParseDuration(ins)
-		require.NoError(t, err, ins)
-		require.EqualFloat64(t, ex, act.Seconds(), ins)
+		t.Run(ins, func(t *testing.T) {
+			act, err := ParseDuration(ins)
+			require.NoError(t, err, ins)
+			require.EqualFloat64(t, ex, act.Seconds(), ins)
+		})
 	}
 }
 
 func TestParseBadDurations(t *testing.T) {
 	in := map[string]string{
-		"P20M":   `Duration must be in the format: P[nD][T[nH][nM][nS]]`, // We don't allow Months (doesn't make sense when converting to duration)
-		"P20Y":   `Duration must be in the format: P[nD][T[nH][nM][nS]]`, // We don't allow Years (doesn't make sense when converting to duration)
-		"P15.5D": `Duration must be in the format: P[nD][T[nH][nM][nS]]`, // Only seconds can be expressed as a decimal
-		"P2H":    `Duration must be in the format: P[nD][T[nH][nM][nS]]`, // "T" must be present to separate days and hours
-		"2DT1H":  `Duration must be in the format: P[nD][T[nH][nM][nS]]`, // "P" must always be present
-		"PT2M1H": `Duration must be in the format: P[nD][T[nH][nM][nS]]`, // Hours must appear before Minutes
-		"P":      `At least one number and designator are required`,      // At least one number and designator are required
-		"-P20H":  `Duration cannot be negative`,                          // Negative duration doesn't make sense
+		"P20M":   `duration must be in the format: P[nD][T[nH][nM][nS]]`, // We don't allow Months (doesn't make sense when converting to duration)
+		"P20Y":   `duration must be in the format: P[nD][T[nH][nM][nS]]`, // We don't allow Years (doesn't make sense when converting to duration)
+		"P15.5D": `duration must be in the format: P[nD][T[nH][nM][nS]]`, // Only seconds can be expressed as a decimal
+		"P2H":    `duration must be in the format: P[nD][T[nH][nM][nS]]`, // "T" must be present to separate days and hours
+		"2DT1H":  `duration must be in the format: P[nD][T[nH][nM][nS]]`, // "P" must always be present
+		"P":      `duration must be in the format: P[nD][T[nH][nM][nS]]`, // At least one number and designator are required
+		"-PT20H": `duration cannot be negative`,                          // Negative duration doesn't make sense
 	}
 	for ins, msg := range in {
-		_, err := ParseDuration(ins)
-		require.EqualError(t, err, msg, fmt.Sprintf("Expected an error for: %s", ins))
+		t.Run(ins, func(t *testing.T) {
+			_, err := ParseDuration(ins)
+			require.EqualError(t, err, msg, fmt.Sprintf("Expected an error for: %s", ins))
+		})
 	}
 }
